2f96afdea005cb8e62958d9ddaaf6892a26f3a92
Jonas Keturka <Jonas@inventi.lt>
2017-02-13 00:05:04 +0200

    Solve problem 25

---
+++ 25.py
@@ -0,0 +1,46 @@
+"""
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain
+1000 digits?
+"""
+from itertools import takewhile
+
+def main():
+    print(solve(1000))
+
+
+def solve(number):
+    fib_gen = fib()
+    return sum(1 for i in
+               takewhile(lambda x: len(str(x)) < number, fib())
+               ) + 1
+
+
+def fib():
+    a = b = 1
+    yield a
+    yield b
+    while True:
+        a, b = b, a + b
+        yield b
+
+if __name__ == "__main__":
+    main()
